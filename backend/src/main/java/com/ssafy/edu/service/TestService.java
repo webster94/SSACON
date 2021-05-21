0eaa6a038e0e6b95197eab2796b5f987aeb8233c
신민호(본인의 이름) <eeqwer123@gmail.com()>
2021-05-21 10:58:57 +0900

    [feat/FE] : master refactoring

--- backend/src/main/java/com/ssafy/edu/service/TestService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.edu.service;
-
-import com.ssafy.edu.model.TestResponse;
-import org.springframework.http.ResponseEntity;
-
-public interface TestService {
-    public ResponseEntity<TestResponse> getDate(String Date);
-    public ResponseEntity<TestResponse> sendDate(Integer pk);
-}
